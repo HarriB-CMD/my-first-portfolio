588df192e6284aa0a510d7c6909d86518b414399
Boateng Nkrumah Harrison <boatengh33@gmail.com>
2026-04-07 16:09:18 +0000

    added some products

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // 1. The list of products
 const products = [
     { name: "Vintage Denim Jacket", price: 45.00, category: "Clothes", img: "images/7.jpg" },
-    { name: "Smart Air Fryer", price: 120.00, category: "Appliances", img: "images/2.jpg" },
+    { name: "Drawer with mirror", price: 120.00, category: "Room Deco", img: "images/2.jpg" },
     { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/3.jpg" },
-    { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/4.jpg" },
-    { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/5.jpg" },
-    { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/10.jpg" },
-    { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/13.jpg" },
+    { name: "Ladies Sandals", price: 35.00, category: "Sandals", img: "images/10.jpg" },
+    { name: "All white Center Table", price: 500.00, category: "Room Deco", img: "images/13.jpg" },
     { name: "Summer Floral Dress", price: 35.00, category: "Clothes", img: "images/18.jpg" },
+    { name: "Summer Bags", price: 35.00, category: "Bags", img: "images/16.jpg" },
+    { name: "Umbrella Spray ", price: 40.00, category: "Cosmetics", img: "images/12.jpg" },
 
     // --- ADD THE SHOES HERE ---
     { 
-        name: "Leather Boots", 
+        name: "Ladies Sandals", 
         price: 85.00, 
-        category: "Shoes", 
+        category: "Sandals", 
         img: "images/4.jpg" 
     },
     { 
